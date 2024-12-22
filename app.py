cc43ce48d87d3090c1a56c6c9be18a0b8448e7a5
Xhemajli07 <xhemajli.krasniqi@gmail.com>
2024-12-22 11:03:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Bienvenue sur l'API edge-tts !", 200
 
 @app.route('/synthesize', methods=['POST'])
 async def synthesize():
